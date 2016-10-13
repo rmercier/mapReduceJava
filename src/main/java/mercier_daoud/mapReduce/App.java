41a1551ed69d109ad99009eba868db7dc9dbfb56
Romain Mercier <romain_mercier24@yahoo.fr>
2016-10-13 17:25:00 +0200

    First and Second Map Reduce exercices

---
+++ src/main/java/mercier_daoud/mapReduce/App.java
@@ -0,0 +1,13 @@
+package mercier_daoud.mapReduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
